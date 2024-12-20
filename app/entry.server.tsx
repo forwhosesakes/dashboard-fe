47e58ad8463ed01b46af10f9ebb85515134c9582
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-20 22:24:53 +0300

    react router pls work

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,6 +2,7 @@ import type { AppLoadContext, EntryContext } from "react-router";
 import { ServerRouter } from "react-router";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
+import React from "react";
 
 const ABORT_DELAY = 5_000;
 
