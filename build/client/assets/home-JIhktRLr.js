d35d1a55516bd8f93d40b38c74e53c7cb0d901df
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-21 20:10:05 +0300

    ignore build

--- build/client/assets/home-JIhktRLr.js
+++
@@ -1,2 +0,0 @@
-import{w as t}from"./with-props-_dSncGax.js";import{n as e}from"./chunk-D52XG6IA-BoJ4nZbF.js";function a({}){return[{title:"New React Router App"},{name:"description",content:"Welcome to React Router!"}]}const i=t(function({loaderData:n}){return e.jsx("h2",{children:"homw"})});export{i as default,a as meta};
-//# sourceMappingURL=home-JIhktRLr.js.map
