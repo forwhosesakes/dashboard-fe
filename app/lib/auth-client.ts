a28aa51fcbef24713996a9681bbc9b7ff36635ce
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-21 22:20:43 +0300

    base url

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,4 +1,11 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server
-})
+
+import { adminClient } from "better-auth/client/plugins"
+
+export const   authClient = createAuthClient({
+            baseURL: "https://dashboard-be.forwhosesakes.workers.dev", //"http://localhost:3000", // the base url of your auth serverm
+            plugins: [
+                adminClient()
+            ]
+        })
+
