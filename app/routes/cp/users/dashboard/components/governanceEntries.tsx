48615384cf8ad6cf47eb76aee1de5160fe645747
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2025-02-18 16:52:03 +0300

    gov

--- app/routes/cp/users/dashboard/components/governanceEntries.tsx
+++ app/routes/cp/users/dashboard/components/governanceEntries.tsx
@@ -2,15 +2,25 @@ import type { TGovernanceEntries } from "~/types/dashboard.types";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { useReducer, useState } from "react";
 import { GOVERANCE_TABS, governanceLabels } from "../constants/glossary";
-import { COMPLIANCE_ADHERENCE_PRACTICES_QUESTIONS } from "../constants/governance";
+import {
+  COMPLIANCE_ADHERENCE_PRACTICES_QUESTIONS,
+  FINANCIAL_SAFETY_PRACTICES_QUESTIONS,
+  TRANSPARENCY_DISCLOSURE_PRACTICES_QUESTIONS,
+} from "../constants/governance";
 import { Separator } from "~/components/ui/separator";
 import { Button } from "~/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 import { Switch } from "~/components/ui/switch";
 import { dashboardApi } from "~/lib/api/dashboard";
 import { toasts } from "~/lib/utils/toast";
-import { useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { useLoaderData } from "react-router";
 
+const TABS_QUESTIONS = {
+  COMPLIANCE_ADHERENCE_PRACTICES: COMPLIANCE_ADHERENCE_PRACTICES_QUESTIONS,
+  FINANCIAL_SAFETY_PRACTICES: FINANCIAL_SAFETY_PRACTICES_QUESTIONS,
+  TRANSPARENCY_DISCLOSURE_PRACTICES:
+    TRANSPARENCY_DISCLOSURE_PRACTICES_QUESTIONS,
+};
 
 const GovernanceEntries = () => {
   const tabs: TGovernanceEntries[] = [
@@ -18,7 +28,7 @@ const GovernanceEntries = () => {
     "FINANCIAL_SAFETY_PRACTICES",
     "TRANSPARENCY_DISCLOSURE_PRACTICES",
   ];
-  const {baseUrl, id } = useLoaderData();
+  const { baseUrl, id } = useLoaderData();
   const [govTab, setGovTab] = useState<TGovernanceEntries>(
     "COMPLIANCE_ADHERENCE_PRACTICES"
   );
@@ -56,50 +66,58 @@ const GovernanceEntries = () => {
   const handleTabChange = (value: string) => {
     setGovTab(value as TGovernanceEntries);
   };
-// Function to check if a question should be answered
-const shouldQuestionBeAnswered = (question: any, sectionQuestions: any[], i: number) => {
-  if (i === 0) return true;
-  if (!question.isDependantOnPrev) return true;
-  
-  const previousQuestion = sectionQuestions[i - 1];
-  const previousResponse = responses[previousQuestion.label];
-  
-  if (!previousResponse) return false;
-  
-  const [value, index] = previousResponse.split('-');
-  return previousQuestion.options[parseInt(index)]?.moveToNext;
-};
+  // Function to check if a question should be answered
+  const shouldQuestionBeAnswered = (
+    question: any,
+    sectionQuestions: any[],
+    i: number
+  ) => {
+    if (i === 0) return true;
+    if (!question.isDependantOnPrev) return true;
 
-// Function to get all required question labels
-const getRequiredQuestions = () => {
-  const required = new Set<string>();
-  
-  Object.values(COMPLIANCE_ADHERENCE_PRACTICES_QUESTIONS).forEach(section => {
-    section.questions.forEach((question, index) => {
-      if (shouldQuestionBeAnswered(question, section.questions, index)) {
-        required.add(question.label);
-      }
+    const previousQuestion = sectionQuestions[i - 1];
+    const previousResponse = responses[previousQuestion.label];
+
+    if (!previousResponse) return false;
+
+    const [value, index] = previousResponse.split("-");
+    return previousQuestion.options[parseInt(index)]?.moveToNext;
+  };
+
+  // Function to get all required question labels
+  const getRequiredQuestions = () => {
+    const required = new Set<string>();
+
+    Object.values(TABS_QUESTIONS[govTab]).forEach((section) => {
+      section.questions.forEach((question, index) => {
+        console.log("question::", question);
+        console.log(
+          "should it be answured::",
+          shouldQuestionBeAnswered(question, section.questions, index)
+        );
+
+        if (shouldQuestionBeAnswered(question, section.questions, index)) {
+          required.add(question.label);
+        }
+      });
     });
-  });
-  
-  return required;
-};
 
-// Check if all required questions are answered
-const hasUnansweredQuestions = () => {
-  const requiredQuestions = getRequiredQuestions();
-  return Array.from(requiredQuestions).some(label => !responses[label]);
-};
-    
+    return required;
+  };
 
+  // Check if all required questions are answered
+  const hasUnansweredQuestions = () => {
+    const requiredQuestions = getRequiredQuestions();
+    return Array.from(requiredQuestions).some((label) => !responses[label]);
+  };
 
   const handleSubmit = async () => {
-    console.log("responses submitted:::", responses)
+    console.log("responses submitted:::", responses);
     if (hasUnansweredQuestions()) {
       setShowValidation(true);
       toasts.error({
         message: "خطأ في تعبئة النموذج",
-        description: "يرجى الإجابة على جميع الأسئلة المطلوبة"
+        description: "يرجى الإجابة على جميع الأسئلة المطلوبة",
       });
       return;
     }
@@ -137,103 +155,104 @@ const hasUnansweredQuestions = () => {
 
   return (
     <>
-
-
       <div className="flex mb-8 gap-x-2 text-gray-500">
-      <Switch dir="ltr" checked={!!isFormMode} onCheckedChange={triggerFormMode} />{"اظهار استبيانات الحوكمة"}
-        </div>
-     {isFormMode&&  <Tabs
-        defaultValue={govTab}
-        dir="rtl"
-        className="w-full mb-4"
-        onValueChange={handleTabChange}
-      >
-     
-        <TabsList className="w-full justify-start bg-transparent">
-          {tabs.map((tab) => (
-            <TabsTrigger value={tab}>{GOVERANCE_TABS[tab]}</TabsTrigger>
-          ))}
-        </TabsList>
+        <Switch
+          dir="ltr"
+          checked={!!isFormMode}
+          onCheckedChange={triggerFormMode}
+        />
+        {"اظهار استبيانات الحوكمة"}
+      </div>
+      {isFormMode && (
+        <Tabs
+          defaultValue={govTab}
+          dir="rtl"
+          className="w-full mb-4"
+          onValueChange={handleTabChange}
+        >
+          <TabsList className="w-full justify-start bg-transparent">
+            {tabs.map((tab) => (
+              <TabsTrigger value={tab}>{GOVERANCE_TABS[tab]}</TabsTrigger>
+            ))}
+          </TabsList>
 
-        <TabsContent value={govTab}>
-         <div className="w-full max-w-4xl mx-auto p-4 space-y-6">
-            {Object.entries(COMPLIANCE_ADHERENCE_PRACTICES_QUESTIONS).map(
-              ([sectionId, section], i) => (
-                <div key={sectionId} className="space-y-4">
-                  <div className="flex items-center gap-2">
-                    <h2 className="text-lg text-secondary-900 font-semibold">
-                      القسم {i + 1}
-                    </h2>
-                    <span className="text-sm text-muted-foreground">
-                      (الدرجة: {section.weight})
-                    </span>
-                  </div>
-                  <Separator />
+          <TabsContent value={govTab}>
+            <div className="w-full max-w-4xl mx-auto p-4 space-y-6">
+              {Object.entries(TABS_QUESTIONS[govTab]).map(
+                ([sectionId, section], i) => (
+                  <div key={sectionId} className="space-y-4">
+                    <div className="flex items-center gap-2">
+                      <h2 className="text-lg text-secondary-900 font-semibold">
+                        القسم {i + 1}
+                      </h2>
+                      <span className="text-sm text-muted-foreground">
+                        (الدرجة: {section.weight})
+                      </span>
+                    </div>
+                    <Separator />
 
-                  {section.questions.map(
-                    (question, qIndex) =>
-                      shouldShowQuestion(
-                        question,
-                        section.questions,
-                        qIndex
-                      ) && (
-                        <div
-                          key={question.label}
-                          className="p-4 border rounded-lg"
-                        >
-                          <p className="text-sm font-medium mb-4">
-                            {qIndex + 1}.{" "}
-                            {
-                              //@ts-ignore
-                              governanceLabels.COMPLIANCE_ADHERENCE_PRACTICES[
-                                question.label
-                              ]
-                            }
-                          </p>
-                          <RadioGroup
-                            dir="rtl"
-                            onValueChange={(value) =>
-                              handleResponse(sectionId, question.label, value)
-                            }
-                            value={responses[question.label]}
-                            className="space-y-2"
+                    {section.questions.map(
+                      (question, qIndex) =>
+                        shouldShowQuestion(
+                          question,
+                          section.questions,
+                          qIndex
+                        ) && (
+                          <div
+                            key={question.label}
+                            className="p-4 border rounded-lg"
                           >
-                            {question.options.map((option, index) => (
-                              <div
-                                key={index}
-                                className="flex items-center space-x-2"
-                              >
-                                <RadioGroupItem
-                                  dir="rtl"
-                                  value={`${option.weight.toString()}-${index}`}
-                                  id={`${question.label}-${index}`}
-                                />
-                                <p>{option.label}</p>
-                                <span className="text-xs text-gray-400">
-                                  {" "}
-                                  (الدرجة: {option.weight})
-                                </span>
-                              </div>
-                            ))}
-                          </RadioGroup>
-                        </div>
-                      )
-                  )}
-                </div>
-              )
-            )}
+                            <p className="text-sm font-medium mb-4">
+                              {qIndex + 1}.{" "}
+                              {
+                                //@ts-ignore
+                                governanceLabels[govTab][question.label]
+                              }
+                            </p>
+                            <RadioGroup
+                              dir="rtl"
+                              onValueChange={(value) =>
+                                handleResponse(sectionId, question.label, value)
+                              }
+                              value={responses[question.label]}
+                              className="space-y-2"
+                            >
+                              {question.options.map((option, index) => (
+                                <div
+                                  key={index}
+                                  className="flex items-center space-x-2"
+                                >
+                                  <RadioGroupItem
+                                    dir="rtl"
+                                    value={`${option.weight.toString()}-${index}`}
+                                    id={`${question.label}-${index}`}
+                                  />
+                                  <p>{option.label}</p>
+                                  <span className="text-xs text-gray-400">
+                                    {" "}
+                                    (الدرجة: {option.weight})
+                                  </span>
+                                </div>
+                              ))}
+                            </RadioGroup>
+                          </div>
+                        )
+                    )}
+                  </div>
+                )
+              )}
 
-            <Button
-              onClick={handleSubmit}
-              className="w-full mt-6"
-              
-              variant={"secondary"}
-            >
-              رفع الاستبيان
-            </Button>
-          </div>
-        </TabsContent>
-      </Tabs>}
+              <Button
+                onClick={handleSubmit}
+                className="w-full mt-6"
+                variant={"secondary"}
+              >
+                رفع الاستبيان
+              </Button>
+            </div>
+          </TabsContent>
+        </Tabs>
+      )}
     </>
   );
 };
